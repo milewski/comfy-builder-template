1209be3ab293534ee439545bd7c1fb23299353b4
Rafael Milewski` <rafael.milewski@gmail.com>
2025-10-18 09:23:50 +0800

    update core to 0.0.7

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub struct Output {
 )]
 pub struct Sum;
 
-impl<'a> Node<'a> for Sum {
+impl Node for Sum {
     type In = Input;
     type Out = Output;
     type Error = Box<dyn Error + Send + Sync>;
